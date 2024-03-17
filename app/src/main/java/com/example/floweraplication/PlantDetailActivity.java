8f3938251a2f1df6e9e4669a6eba2af0e88509f3
gadina <jacquefresssco@gmail.com>
2024-03-17 17:06:45 +0300

    Карточка растения

--- app/src/main/java/com/example/floweraplication/PlantDetailActivity.java
+++ app/src/main/java/com/example/floweraplication/PlantDetailActivity.java
@@ -2,13 +2,19 @@ package com.example.floweraplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import androidx.appcompat.widget.Toolbar;
 import com.bumptech.glide.Glide;
 import com.example.floweraplication.databinding.ActivityAdminButonsBinding;
 import com.example.floweraplication.databinding.ActivityPlantDetailBinding;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class PlantDetailActivity extends AppCompatActivity {
 
@@ -16,12 +22,23 @@ public class PlantDetailActivity extends AppCompatActivity {
 
     TextView PlName,PlText,PlType,PlHabitat,PlSize,PlEndurance,PlPurpose,PlTox;
     ImageView PlImage;
+    AppBarLayout toolbar;
 
+   Toolbar tlb;
+
+    @SuppressLint("RestrictedApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityPlantDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        tlb=binding.toolbar;
+        setSupportActionBar(tlb);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
         PlName = binding.PlName;
         PlText = binding.PlText;
